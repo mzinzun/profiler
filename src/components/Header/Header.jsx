23ed5efa799c27b84318af1cb71958fbff40396b
mzinzun <zinzunmichael@gmail.com>
2023-05-10 17:49:17 -0700

    Added Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+const React = require('react');
+
+function Header(props){
+    return(
+        <div className="header">
+            <h1>Michael Zinzun<span className="title">Software Engineer</span></h1>
+        </div>
+    )
+}
+export default Header;
