c8624bbcff38e56b9564ec19cabe608aae80b35b
mzinzun <zinzunmichael@gmail.com>
2023-05-14 23:32:29 -0700

    adding menu

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,32 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Header from "../components/Header";
-import Home from "../components/Home"
+import Home from "../components/Home";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <main>
-        <Home />
-      </main>
-      <footer>footer</footer>
+      <Router>
+        <Header />
+        <nav>
+          <ul>
+            <Link to='/home'><li>Home</li></Link>
+            <Link to='/bio'><li>Bio</li></Link>
+            <Link to='/resume'><li>Resume</li></Link>
+          </ul>
+        </nav>
+
+
+
+        <main>
+          <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/bio" element={<Header />} />
+          </Routes>
+        </main>
+        <footer>footer</footer>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
-
+import {Link} from 'react-router-dom';
 import './Header.css'
 
 function Header(props){
     return(
         <header className="header">
-            <h1>Michael Zinzun</h1>
-            <h2>Software Engineer</h2>
+            <div>
+                <h1>Michael Zinzun</h1>
+                <h2>Software Engineer</h2>
+            </div>
+           
         </header>
     )
 }
