cb504d198d1f2941e21b0d6304251b68a85babf3
mzinzun <zinzunmichael@gmail.com>
2023-05-15 17:55:07 -0700

    working on navbar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,27 +2,24 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Header from "../components/Header";
 import Home from "../components/Home";
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
         <nav>
-          <ul>
+          <ul className='menu navbar justify-content-space-evenly;'>
             <Link to='/home'><li>Home</li></Link>
             <Link to='/bio'><li>Bio</li></Link>
             <Link to='/resume'><li>Resume</li></Link>
+            <Link to='/resume'><li>Contact</li></Link>
           </ul>
         </nav>
-
-
-
         <main>
           <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/bio" element={<Header />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/bio" element={<Header />} />
           </Routes>
         </main>
         <footer>footer</footer>
