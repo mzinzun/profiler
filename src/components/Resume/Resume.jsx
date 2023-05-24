3a8fb756e84919608e0f6cf67b90bd0c0cd0ef03
mzinzun <zinzunmichael@gmail.com>
2023-05-23 21:35:17 -0700

    changed pdfjs.GlobalWorkerOptions.workerSrc

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -7,6 +7,7 @@ import './Resume.css';
 import resume from './mzResume.pdf';
 
 import { PDFDocumentProxy } from 'pdfjs-dist';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 // pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 //   'pdfjs-dist/build/pdf.worker.min.js',
