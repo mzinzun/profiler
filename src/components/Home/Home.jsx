725c4ee4ad1a68c2d0b08c0dbaed77747492537c
mzinzun <zinzunmichael@gmail.com>
2023-05-26 21:07:04 -0700

    added sample site screenshots - inactive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import "./Home.css";
+import site1 from "./fontawesome.png";
+import site2 from "./mikesRemodel.png";
+import site3 from "./capstone.png";
+import site4 from "./zincom.png";
 
 function Home(props) {
+
     const [homeOpacity, setHomeOpacity] = useState(0);
     useEffect(() => {
         console.log('Home Component is Loaded');
@@ -10,8 +15,34 @@ function Home(props) {
         props.setlogosOpacity(1);
     }, [])
     return (
-        <div className='home' style={{opacity: homeOpacity}}>
-        </div>
+        <>
+            <div className='home' style={{ opacity: homeOpacity }}>
+
+                <div className="sampleSites row m-0 p-0 ">
+                    <section className="col-md-3">
+                        <h3>FontAwesome App</h3>
+                        <img src={site1} className='sites' />
+                    </section>
+                    <section className="col-md-3">
+                        <h3>Mikes Remodeling</h3>
+                        <img src={site2} className='sites' />
+                    </section>
+                    <section className="col-md-3">
+                        <h3>Zincom Web</h3>
+                        <img src={site3} className='sites' />
+                    </section>
+                    <section className="col-md-3">
+                        <h3>Zincom CS</h3>
+                        <img src={site4} className='sites' />
+                    </section>
+                </div>
+            </div>
+        </>
+
     )
 }
 export default Home;
+
+function SampleSites(props) {
+
+}
