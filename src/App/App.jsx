f81493a3e444c9010e45e17a690b24669be91f5f
mzinzun <zinzunmichael@gmail.com>
2023-06-01 15:11:09 -0700

    more mobile fixes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -54,8 +54,8 @@ function App(props) {
                     </div>
                 </main>
                 <footer className="row m-0 p-0 px-2 justify-content-between align-items-center">
-                    <h6 className="footerText col-9">copyright &copy;2023 - Designed by Michael Zinzun</h6>
-                    <div className="col-3 text-end">
+                    <h6 className="footerText col-8">copyright &copy;2023 - Designed by Michael Zinzun</h6>
+                    <div className="col-4 text-end">
                         <Link to="https://linkedin.com/in/michaelzinzun" target="_blank"><i className="fab fa-linkedin fa-2x mx-1"></i></Link>
                         <Link to="https://github.com/mzinzun" target="_blank"><i className="fa-brands fa-github fa-2x mx-1"></i></Link>
 
