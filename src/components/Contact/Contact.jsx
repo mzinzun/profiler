4f6396c022f90de01c80142e8cca80fe93cfa4f7
mzinzun <zinzunmichael@gmail.com>
2023-06-11 13:51:35 -0700

    changed Home Image

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,26 @@
-import {useEffect,useState} from 'react';
+import { useEffect, useState } from 'react';
 import './Contact.css';
 
-function Contact(props){
-    useEffect(()=>{
+function Contact(props) {
+    useEffect(() => {
         console.log('Contact Component Loaded');
         props.setlogosOpacity(.2);
 
-    },[])
+    }, [])
     return (
-        <>
+        <div className="contactPage">
             <h1>Contact Page</h1>
-        </>
+            <div className="contactHeader">
+
+            </div>
+            <form className="form d.flex flex-col border  w-50">
+                <div className="form-group col">
+                    <label htmlFor="firstName">First Name</label>
+                    <input name="firstName" />
+                </div>
+                
+            </form>
+        </div>
     )
 }
 
